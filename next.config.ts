a412de70bb3bcc36a0acdd09fa7b729b281c2ffd
SEVNGYVN <icheon.s230043@ggh.goe.go.kr>
2026-04-01 17:20:48 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',        // 빌드 결과물을 HTML 파일로 뽑아내겠다는 설정 (필수!)
-  basePath: '/lsy_blog',   // 주소 뒤에 저장소 이름이 붙으므로 경로를 맞춰줍니다.
+TypeScript
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* 빌드 결과물을 정적 HTML로 추출 (GitHub Pages 필수 설정) */
+  output: 'export',
+
+  /* 저장소 이름이 주소 뒤에 붙으므로 경로를 맞춰줍니다 */
+  basePath: '/lsy_blog',
+
+  /* 정적 배포 시 이미지 최적화 기능을 꺼야 빌드 오류가 안 납니다 */
   images: {
-    unoptimized: true,     // 정적 배포 시 Next.js의 이미지 최적화 기능을 꺼야 합니다.
+    unoptimized: true,
   },
 };
 
